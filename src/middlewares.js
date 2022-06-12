5fe79dc3081fb4d677d0da1c03a0d7a634826adf
GimokYun <ygm5239@gmail.com>
2022-06-13 02:55:54 +0900

    revise middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,15 @@ const s3 = new aws.S3({
     }
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
     s3: s3,
-    bucket: "wimm-ygm",
+    bucket: "wimm-ygm/images",
+    acl: "public-read"
+});
+
+const s3VideoUploader = multerS3({
+    s3: s3,
+    bucket: "wimm-ygm/videos",
     acl: "public-read"
 });
 
@@ -44,7 +50,7 @@ export const uploadAvatars = multer({
     limits: {
         fileSize: 5000000
     },
-    storage: multerUploader,
+    storage: s3ImageUploader,
 });
 
 export const uploadVideos = multer({
@@ -52,5 +58,5 @@ export const uploadVideos = multer({
     limits: {
         fileSize: 10000000
     },
-    storage: multerUploader,
+    storage: s3VideoUploader,
 });
